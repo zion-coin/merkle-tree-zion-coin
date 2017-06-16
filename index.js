5700852bc8b8adbf35eb0bb874249c3707eb1ef7
Craig OConnor <oconnorct1@gmail.com>
2017-06-16 14:28:06 -0600

    up

---
+++ index.js
@@ -0,0 +1,59 @@
+const SHA3   = require('sha3');
+
+const arr = ["a", "b", "c", "d"];
+
+class merkleTree {
+  constructor(input) {
+    const self = this;
+
+    self.root  = null;
+    self.tree  = {};
+    self.depth = Math.ceil(Math.log2(input.length));
+
+    //Now we can create the tree
+    for (let x = self.depth; x >= 0; x--) {
+      // verify that the merkle tree is even in number
+      // if not, the last one will have to merge with itself
+      if (input.length % 2 !== 0)
+        input.push(input[input.length - 1]);
+
+      if (x === self.depth) {
+        input = input.map(doubleSha3);
+      } else {
+        let first;
+        input = input.map((hash, i) => {
+            if (i % 2 === 0) {
+              first = hash;
+              return null
+            } else {
+              return doubleSha3(first + hash);
+            }
+          })
+          .filter((hash) => { return hash !== null });
+      }
+      self.tree[x] = input;
+    }
+    // The root is at the top
+    self.root = self.tree[0][0];
+  }
+
+  digest() {
+    return this.root;
+  }
+}
+
+function doubleSha3(payload) {
+  if (typeof payload === 'string')
+    payload = Buffer.from(payload);
+
+  return new SHA3.SHA3Hash()
+                  .update(
+                    new SHA3.SHA3Hash()
+                            .update(payload)
+                            .digest()
+                  )
+                  .digest('hex');
+}
+
+let mT = new merkleTree(arr);
+console.log(mT);
---
+++ test.js
@@ -0,0 +1,14 @@
+// console.log("doubleSha3 of a", doubleSha3("a"));
+// // e55ba81e90c44da0f18f97b7725c03dd7988f405a42ee40bccf4191c75d1689b021a8cf38b71e1018981ec695ba6bf6ddccdda6a1e11a2d499a3b224a55f5884
+// console.log("doubleSha3 of b", doubleSha3("b"));
+// // 6991820944362c2f3ff0df9d76df45988ac49576afcf2c3de52b6fdbd498bde9cef0f8331f0c764a7cc0d79e31936488041e1ed10e853a0f82f13f846f6766e7
+// console.log("doubleSha3 of c", doubleSha3("c"));
+// // 9f1d203b5e69b1a28c636d8dabe28d5d204d92716eb14ea4f06e96010bc5d36324ce09d339026507aaf0667474b299d8ed508bb733c2c85062a7e0d5a24fdccd
+// console.log("doubleSha3 of d", doubleSha3("d"));
+// // e71003603d91e84dbd6fc36c49e7b42ce52922628757f7825394c71c07a4b0e42cd3a66aab93dc772f174663c762121ba9db5ecbca4113934bc48433507dc169
+// console.log("doubleSha3 of ab", doubleSha3(doubleSha3("a") + doubleSha3("b")));
+// // 71e44c3a25fd757c86ffe8884eadfeabb707155319e969dd7179064a75b43931e853c37f6a327b7670a0bedf6a1f3f6ee2ed3fff9f2b6cce47d08b2a259b3c13
+// console.log("doubleSha3 of cd", doubleSha3(doubleSha3("c") + doubleSha3("d")));
+// // a8be8d503ac9b8ddca923440ecc5cdf743489a294e6fdcec3bc730e364cbbd72c38e3ac4dee227755b85ad79c63334143dca20309e1eb98053b5e30822e596d6
+// console.log("doubleSha3 of abcd", doubleSha3( doubleSha3(doubleSha3("a") + doubleSha3("b")) + doubleSha3(doubleSha3("c") + doubleSha3("d")) ));
+// // 1faf78938bc66bf5c824f97550662157ab3cefcc74ef282f3d29b98248395da44825fe851c14900f9b516649d6151e4e75791a9a36360129f549fd80d314a617
